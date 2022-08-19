970db8ac35d923d6e9ec722285479bffcac3cb79
Rob Ahnemann <rob.ahnemann@gmail.com>
2022-08-19 15:02:18 -0500

    Updating build to be dynamic with SDK

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,14 @@
 
 #Depending on the operating system of the host machines(s) that will build or run the containers, the image specified in the FROM statement may need to be changed.
 #For more information, please see https://aka.ms/containercompat
-ARG BASE_IMAGE=mcr.microsoft.com/dotnet/runtime:6.0-nanoserver-1809
+ARG BASE_IMAGE  
+ARG BUILD_IMAGE
 
-FROM mcr.microsoft.com/dotnet/runtime:6.0 AS base
+FROM ${BASE_IMAGE} AS base
 USER ContainerAdministrator
 WORKDIR /app
 
-FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+FROM ${BUILD_IMAGE} AS build
 USER ContainerAdministrator
 WORKDIR /src
 COPY ["windows-hosts-writer.csproj", "."]
--- bagandtag.bat
+++ bagandtag.bat
@@ -1,5 +1,5 @@
-docker build -t rahnemann/windows-hosts-writer:2.0-nanoserver-1809 --build-arg BASE_IMAGE=mcr.microsoft.com/dotnet/runtime:6.0-nanoserver-1809 .
-docker build -t rahnemann/windows-hosts-writer:2.0-nanoserver-1909 --build-arg BASE_IMAGE=mcr.microsoft.com/dotnet/runtime:6.0-nanoserver-1909 .
-docker build -t rahnemann/windows-hosts-writer:2.0-nanoserver-2004 --build-arg BASE_IMAGE=mcr.microsoft.com/dotnet/runtime:6.0-nanoserver-2004 .
-docker build -t rahnemann/windows-hosts-writer:2.0-nanoserver-20H2 --build-arg BASE_IMAGE=mcr.microsoft.com/dotnet/runtime:6.0-nanoserver-20H2 .
-docker build -t rahnemann/windows-hosts-writer:2.0-nanoserver-ltsc2022 --build-arg BASE_IMAGE=mcr.microsoft.com/dotnet/runtime:6.0-nanoserver-ltsc2022 .
+docker build -t rahnemann/windows-hosts-writer:2.0-nanoserver-1809 --build-arg BASE_IMAGE=mcr.microsoft.com/dotnet/runtime:6.0-nanoserver-1809 --build-arg BUILD_IMAGE=mcr.microsoft.com/dotnet/sdk:6.0-nanoserver-1809 .
+docker build -t rahnemann/windows-hosts-writer:2.0-nanoserver-1909 --build-arg BASE_IMAGE=mcr.microsoft.com/dotnet/runtime:6.0-nanoserver-1909 --build-arg BUILD_IMAGE=mcr.microsoft.com/dotnet/sdk:6.0-nanoserver-1909 .
+docker build -t rahnemann/windows-hosts-writer:2.0-nanoserver-2004 --build-arg BASE_IMAGE=mcr.microsoft.com/dotnet/runtime:6.0-nanoserver-2004 --build-arg BUILD_IMAGE=mcr.microsoft.com/dotnet/sdk:6.0-nanoserver-2004 .
+docker build -t rahnemann/windows-hosts-writer:2.0-nanoserver-20H2 --build-arg BASE_IMAGE=mcr.microsoft.com/dotnet/runtime:6.0-nanoserver-20H2 --build-arg BUILD_IMAGE=mcr.microsoft.com/dotnet/sdk:6.0-nanoserver-20H2 .
+docker build -t rahnemann/windows-hosts-writer:2.0-nanoserver-ltsc2022 --build-arg BASE_IMAGE=mcr.microsoft.com/dotnet/runtime:6.0-nanoserver-ltsc2022 --build-arg BUILD_IMAGE=mcr.microsoft.com/dotnet/sdk:6.0-nanoserver-ltsc2022 .
